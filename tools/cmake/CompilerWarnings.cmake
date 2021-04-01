eda5aba306e394c7b521ef499d8f2629ef4efa36
Henrik S. Gaßmann <henrik@gassmann.onl>
2021-04-01 07:37:01 +0200

    Disable `-Wuseless-cast`
    `-Wuseless-cast` produces too many false positives, because it flags
    downcasts from `uint64_t` to `size_t`. If we removed these casts we
    would trigger unsafe implicit narrowing warnings on 32Bit platforms. Do
    note that in all cases `std::in_range()` is employed to prevent
    information loss.

--- tools/cmake/CompilerWarnings.cmake
+++ tools/cmake/CompilerWarnings.cmake
@@ -62,7 +62,7 @@ set(GCC_WARNINGS
     -Wduplicated-cond # warn if if / else chain has duplicated conditions
     -Wduplicated-branches # warn if if / else branches have duplicated code
     -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-    -Wuseless-cast # warn if you perform a cast to the same type
+    #-Wuseless-cast # warn if you perform a cast to the same type
 )
 
 # the warning are not added to the library target in order to allow
