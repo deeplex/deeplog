e9edf9330ef20734ee3b5fcb820fe220727bb24d
Henrik S. Gaßmann <henrik@gassmann.onl>
2021-08-26 18:10:56 +0200

    Add first TUI experiments

--- apps/deeplog-cli/src/deeplog.main.cpp
+++ apps/deeplog-cli/src/deeplog.main.cpp
@@ -1,4 +1,203 @@
 
+#include <ranges>
+
+#include <fmt/format.h>
+
+#include <parallel_hashmap/phmap.h>
+
+#include <dplx/dlog/definitions.hpp>
+#include <dplx/dlog/file_database.hpp>
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/node.hpp>
+
+namespace dplx::dlog::tui
+{
+
+struct options
+{
+    phmap::flat_hash_map<std::string, bool> enabled_containers;
+};
+
+class OptionsComponent : public ftxui::ComponentBase
+{
+    file_database_handle &mFileDb;
+    options &mValue;
+
+    ftxui::Component mFileSelection;
+
+    phmap::flat_hash_map<std::string, bool> mEnabledContainersBuilder;
+
+public:
+    OptionsComponent(file_database_handle &fileDb, options &value)
+        : ComponentBase()
+        , mFileDb(fileDb)
+        , mValue(value)
+        , mFileSelection(ftxui::Container::Vertical({}))
+    {
+        UpdateFileSelection();
+        Add(ftxui::Container::Vertical({
+                ftxui::Renderer(mFileSelection,
+                                [this]
+                                {
+                                    return ftxui::window(
+                                            ftxui::text("enabled sinks"),
+                                            mFileSelection->Render());
+                                }),
+        }));
+    }
+
+    auto Render() -> ftxui::Element override
+    {
+
+        return ComponentBase::Render();
+    }
+
+private:
+    void UpdateFileSelection()
+    {
+        auto const &recordContainers = mFileDb.record_containers();
+
+        phmap::flat_hash_map<file_sink_id, ftxui::Component> resourcesView;
+
+        for (auto const &container : recordContainers)
+        {
+            bool enabled = true;
+            auto key = container.path.generic_string();
+            if (auto state = mValue.enabled_containers.find(key);
+                state != mValue.enabled_containers.end())
+            {
+                enabled = state->second;
+            }
+
+            auto itsertet = mEnabledContainersBuilder
+                                    .insert({std::move(key), enabled})
+                                    .first;
+
+            ftxui::Component *conti;
+            if (auto resIt = resourcesView.find(container.sink_id);
+                resIt != resourcesView.end())
+            {
+                conti = &resIt->second;
+            }
+            else
+            {
+                conti = &resourcesView
+                                 .insert({container.sink_id,
+                                          ftxui::Container::Vertical({})})
+                                 .first->second;
+            }
+
+            (*conti)->Add(ftxui::Checkbox(&itsertet->first, &itsertet->second));
+        }
+
+        mFileSelection->DetachAllChildren();
+        for (auto &sink : resourcesView)
+        {
+            auto name = fmt::format("sink {}", sink.first);
+            mFileSelection->Add(ftxui::Renderer(
+                    sink.second,
+                    [name = std::move(name), component = sink.second] {
+                        return ftxui::window(ftxui::text(name),
+                                             component->Render());
+                    }));
+        }
+
+        std::ranges::swap(mValue.enabled_containers, mEnabledContainersBuilder);
+    }
+};
+
+class LogDisplayComponent : public ftxui::ComponentBase
+{
+    file_database_handle &mFileDb;
+    options &mOptions;
+
+    ftxui::Component mLogGrid;
+
+public:
+    LogDisplayComponent(file_database_handle &fileDb, options &opts)
+        : ComponentBase()
+        , mFileDb(mFileDb)
+        , mOptions(opts)
+        , mLogGrid()
+    {
+        Add(mLogGrid);
+    }
+
+
+};
+
+class MainComponent : public ftxui::ComponentBase
+{
+    file_database_handle mFileDb;
+
+    options mOptions;
+
+    int mTabSelector;
+    std::vector<std::string> mTabs;
+    ftxui::Component mTabToggle;
+
+    ftxui::Component mOptionsComponent;
+    ftxui::Component mLogDisplayComponent;
+
+public:
+    MainComponent(file_database_handle &&fileDb)
+        : ComponentBase()
+        , mFileDb(std::move(fileDb))
+        , mOptions{}
+        , mTabSelector(0)
+        , mTabs{"Options", "Log"}
+        , mTabToggle(ftxui::Toggle(&mTabs, &mTabSelector))
+        , mOptionsComponent(
+                  std::make_shared<OptionsComponent>(mFileDb, mOptions))
+        , mLogDisplayComponent(std::make_shared<LogDisplayComponent>())
+    {
+        Add(ftxui::Container::Vertical(
+                {mTabToggle, ftxui::Container::Tab(
+                                     {
+                                             mOptionsComponent,
+                                             mLogDisplayComponent,
+                                     },
+                                     &mTabSelector)}));
+    }
+
+    auto Render() -> ftxui::Element override
+    {
+        ftxui::Element detail
+                = ftxui::text("u broke the main component render :(");
+
+        if (mTabSelector == 0)
+        {
+            detail = mOptionsComponent->Render();
+        }
+        else if (mTabSelector == 1)
+        {
+        }
+
+        return ftxui::vbox({mTabToggle->Render(), ftxui::separator(), detail});
+    }
+};
+
+} // namespace dplx::dlog::tui
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
+    using namespace ftxui;
+    using namespace dplx;
+    namespace llfio = dlog::llfio;
+
+    if (argc < 2)
+    {
+        return -1;
+    }
+
+    llfio::path_view dbPath(argv[1]);
+    auto db = dlog::file_database_handle::file_database({}, dbPath, "").value();
+
+    auto screen = ScreenInteractive::Fullscreen();
+    auto mainComponent
+            = std::make_shared<dlog::tui::MainComponent>(std::move(db));
+    screen.Loop(mainComponent);
 }
