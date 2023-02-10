80a785ce185bbcaadd8067de3ee9ae57c972e34f
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-02-10 16:48:43 +0100

    test: Migrate the ringbus.test to to Catch2

--- src/dplx/dlog/log_bus.test.cpp
+++ src/dplx/dlog/log_bus.test.cpp
@@ -74,6 +74,57 @@ TEST_CASE("bufferbus buffers messages and outputs them afterwards")
     CHECK(endId == msgId);
 }
 
+TEST_CASE("ringbus_mt buffers messages and outputs them afterwards")
+{
+    auto createRx = dlog::ringbus(llfio::mapped_temp_inode().value(), 1, 4);
+    REQUIRE(createRx);
+
+    auto bufferbus = std::move(createRx).assume_value();
+
+    auto msgId = 0U;
+    dlog::ringbus_mt_handle::logger_token token{};
+    for (;;)
+    {
+        auto size = static_cast<unsigned>(dp::encoded_size_of(msgId));
+        if (auto writeRx = bufferbus.write(token, size); writeRx.has_value())
+        {
+            auto encodeRx = dp::encode(writeRx.assume_value(), msgId);
+            REQUIRE(encodeRx);
+
+            bufferbus.commit(token);
+            msgId += 1;
+        }
+        else if (writeRx.assume_error() == dlog::errc::not_enough_space)
+        {
+            break;
+        }
+        else
+        {
+            REQUIRE(writeRx);
+        }
+    }
+
+    auto const endId = msgId;
+    msgId = 0;
+
+    auto consumeRx = bufferbus.consume_content(
+            [&](std::span<std::byte const> msg)
+            {
+                dp::memory_view msgBuffer(msg);
+
+                auto decodeRx
+                        = dp::decode(dp::as_value<unsigned int>, msgBuffer);
+                REQUIRE(decodeRx);
+
+                auto parsedId = decodeRx.assume_value();
+                REQUIRE(parsedId == msgId);
+                msgId += 1;
+            });
+
+    REQUIRE(consumeRx);
+    CHECK(endId == msgId);
+}
+
 } // namespace dlog_tests
 
 // NOLINTEND(readability-function-cognitive-complexity)
