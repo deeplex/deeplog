70c411c13e783d5b7da207e30b9a767906e7172c
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-02-10 16:53:39 +0100

    test: Migrate file_database.test to to Catch2

--- src/dplx/dlog/file_database.test.cpp
+++ src/dplx/dlog/file_database.test.cpp
@@ -6,3 +6,63 @@
 //           https://www.boost.org/LICENSE_1_0.txt)
 
 #include "dplx/dlog/file_database.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "test_dir.hpp"
+#include "test_utils.hpp"
+
+namespace dlog_tests
+{
+
+TEST_CASE("file database API integration test")
+{
+    auto const dbName = llfio::utils::random_string(4U);
+    auto const dbFullName
+            = std::string{dbName}.append(dlog::file_database_handle::extension);
+    auto const sinkFilePattern = dbName + ".{ctr}_{iso8601}.blog";
+
+    auto createRx = dlog::file_database_handle::file_database(
+            test_dir, dbFullName, sinkFilePattern);
+    REQUIRE(createRx);
+    auto &&db = std::move(createRx).assume_value();
+
+    auto create2Rx = db.create_record_container();
+    REQUIRE(create2Rx);
+
+    // cleanup
+    (void)create2Rx.assume_value().close();
+    auto cleanupRx = db.unlink_all();
+    CHECK(cleanupRx);
+}
+
+TEST_CASE("file database reopen round trip")
+{
+    auto const dbName = llfio::utils::random_string(4U);
+    auto const dbFullName
+            = std::string{dbName}.append(dlog::file_database_handle::extension);
+    auto const sinkFilePattern = dbName + ".{ctr}_{iso8601}.blog";
+
+    {
+        auto createRx = dlog::file_database_handle::file_database(
+                {}, dbFullName, sinkFilePattern);
+        REQUIRE(createRx);
+        auto &&db = std::move(createRx).assume_value();
+
+        auto create2Rx = db.create_record_container();
+        REQUIRE(create2Rx);
+    }
+
+    {
+        auto createRx = dlog::file_database_handle::file_database(
+                {}, dbFullName, sinkFilePattern);
+        REQUIRE(createRx);
+        auto &&db = std::move(createRx).assume_value();
+
+        // cleanup
+        auto cleanupRx = db.unlink_all();
+        CHECK(cleanupRx);
+    }
+}
+
+} // namespace dlog_tests
