a0955dc9f66e6097031634e183bccf9efac2c1e4
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-02-10 16:15:06 +0100

    test: Migrate the API integration test to Catch2

--- src/dplx/dlog.test.cpp
+++ src/dplx/dlog.test.cpp
@@ -12,13 +12,57 @@
 #include <catch2/reporters/catch_reporter_registrars.hpp>
 #include <fmt/format.h>
 
+#include <dplx/dlog/definitions.hpp>
+#include <dplx/dlog/file_database.hpp>
 #include <dplx/dlog/llfio.hpp>
+#include <dplx/dlog/sink.hpp>
+#include <dplx/dlog/source.hpp>
 
 #include "test_dir.hpp"
 #include "test_utils.hpp"
 
 namespace dlog_tests
 {
+
+TEST_CASE("The library can create a new database, as new file_sink and write a "
+          "record into it")
+{
+    using sink_type = dlog::basic_sink_frontend<dlog::file_sink_backend>;
+
+    auto dbOpenRx = dlog::file_database_handle::file_database(
+            test_dir, "log-test.drot", "log-test.{iso8601}.dlog");
+    REQUIRE(dbOpenRx);
+    auto &&db = std::move(dbOpenRx).assume_value();
+
+    constexpr auto bufferSize = 64 * 1024;
+    auto sinkBackendOpenRx = dlog::file_sink_backend::file_sink(
+            db.create_record_container(dlog::file_sink_id::default_,
+                                       dlog::file_sink_backend::file_mode)
+                    .value(),
+            bufferSize, {});
+    REQUIRE(sinkBackendOpenRx);
+
+    auto sinkOwner = std::make_unique<sink_type>(
+            dlog::severity::info, std::move(sinkBackendOpenRx).assume_value());
+    auto *sink = sinkOwner.get();
+
+    constexpr auto blockPerWriter = 1 << 10;
+    dlog::core core{dlog::ringbus(test_dir, "tmp", blockPerWriter).value()};
+    core.attach_sink(std::move(sinkOwner));
+
+    dlog::logger xlog{core.connector()};
+    DLOG_GENERIC(xlog, dlog::severity::warn,
+                 u8"important msg with arg {} and {}", 1, dlog::arg("argx", 2));
+
+    auto retireRx = core.retire_log_records();
+    REQUIRE(retireRx);
+    CHECK(retireRx.assume_value() == 0);
+
+    core.release_sink(sink);
+    sinkOwner.reset(sink);
+    auto finRx = sinkOwner->backend().finalize();
+    REQUIRE(finRx);
+}
 
 // we don't want to throw from within an initializer
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
