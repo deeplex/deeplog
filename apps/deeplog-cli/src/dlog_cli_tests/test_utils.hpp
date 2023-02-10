4cc610b9f1dae33ce853795e2469c1583a35f84c
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-02-10 14:17:55 +0100

    feat: Add a Catch2 test project for the CLI

---
+++ apps/deeplog-cli/src/dlog_cli_tests/test_utils.hpp
@@ -0,0 +1,25 @@
+
+// Copyright Henrik S. Gaßmann 2021-2023
+//
+// Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <dplx/predef/compiler.h>
+
+#ifdef DPLX_COMP_GNUC_AVAILABLE
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#endif
+
+namespace dplx
+{
+}
+
+namespace dlog_cli_tests
+{
+
+using namespace dplx;
+
+}
