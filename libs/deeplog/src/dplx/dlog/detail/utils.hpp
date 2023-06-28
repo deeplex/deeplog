0b541c616b6a7d2156566a72ecab37936a2398ba
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-06-28 10:26:43 +0200

    refactor: Remove upstreamed utilities

--- libs/deeplog/src/dplx/dlog/detail/utils.hpp
+++ libs/deeplog/src/dplx/dlog/detail/utils.hpp
@@ -7,66 +7,6 @@
 
 #pragma once
 
-#include <concepts>
-#include <cstddef>
-#include <cstdint>
-#include <limits>
-#include <type_traits>
-
-namespace dplx::dlog::detail
-{
-
-template <typename T, typename... Ts>
-concept any_of = (std::same_as<T, Ts> || ...);
-
-template <typename T, typename... Ts>
-concept none_of = (!std::same_as<T, Ts> && ...);
-
-template <typename T>
-concept integer = std::integral<T>
-               && detail::none_of<std::remove_cv_t<T>,
-                                  bool,
-                                  char,
-                                  wchar_t,
-                                  char8_t,
-                                  char16_t,
-                                  char32_t>;
-
-template <typename T>
-concept signed_integer = integer<T> && std::is_signed_v<T>;
-
-template <typename T>
-concept unsigned_integer = integer<T> && !
-signed_integer<T>;
-
-template <unsigned_integer T, unsigned_integer U>
-constexpr auto div_ceil(T dividend, U divisor) noexcept
-        -> std::common_type_t<T, U>
-{
-    return dividend / divisor + (dividend % divisor != 0);
-}
-
-template <unsigned_integer T>
-constexpr auto make_mask(int num, int off = 0) noexcept -> T
-{
-    constexpr auto digits = std::numeric_limits<T>::digits;
-    return (((static_cast<T>(num < digits)) << (num & (digits - 1))) - 1U)
-        << off;
-}
-
-template <typename T>
-using remove_cref_t = std::remove_const_t<std::remove_reference_t<T>>;
-
-template <typename Enum>
-    requires std::is_enum_v<Enum>
-constexpr auto to_underlying(Enum value) noexcept ->
-        typename std::underlying_type<Enum>::type
-{
-    return static_cast<std::underlying_type_t<Enum>>(value);
-}
-
-} // namespace dplx::dlog::detail
-
 // NOLINTBEGIN(cppcoreguidelines-macro-usage)
 // type arguments can't be enclosed in parentheses
 // NOLINTBEGIN(bugprone-macro-parentheses)
