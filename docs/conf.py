ca04020abb1a155a1bfaa3f152ea311f793bc7c0
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-07-12 11:18:53 +0200

    build: Open version 0.0.0-alpha.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021-2023, Henrik S. Gaßmann'
 author = 'Henrik S. Gaßmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0-alpha.3'
+release = '0.0.0-alpha.4'
 
 
 # -- General configuration ---------------------------------------------------
