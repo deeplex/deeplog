596bf464bbddf1753e4f594d740b22edbe3ec68a
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-08-25 07:47:20 +0200

    feat: Allow resyncing the system clock epoch

--- libs/deeplog/src/dplx/dlog/core/log_clock.hpp
+++ libs/deeplog/src/dplx/dlog/core/log_clock.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include <atomic>
 #include <chrono>
 #include <cstdint>
 
@@ -18,6 +19,8 @@ namespace dplx::dlog
 
 class log_clock
 {
+    using system_clock = std::chrono::system_clock;
+
 public:
     using internal_clock
             = std::conditional_t<std::chrono::high_resolution_clock::is_steady,
@@ -26,13 +29,12 @@ public:
 
     struct epoch_info
     {
-        std::chrono::system_clock::time_point system_reference;
+        system_clock::time_point system_reference;
         internal_clock::time_point steady_reference;
 
         epoch_info() = default;
-        explicit epoch_info(
-                std::chrono::system_clock::time_point systemReference,
-                internal_clock::time_point steadyReference) noexcept
+        explicit epoch_info(system_clock::time_point systemReference,
+                            internal_clock::time_point steadyReference) noexcept
             : system_reference(systemReference)
             , steady_reference(steadyReference)
         {
@@ -60,7 +62,32 @@ public:
     };
 
 private:
-    static epoch_info const epoch_;
+    struct global_epoch_info
+    {
+        std::atomic<system_clock::rep> system_reference;
+        internal_clock::time_point steady_reference;
+
+        explicit global_epoch_info(
+                system_clock::time_point systemReference,
+                internal_clock::time_point steadyReference) noexcept;
+
+        [[nodiscard]] auto get_system_reference() const noexcept
+                -> system_clock::time_point
+        {
+            return system_clock::time_point(system_clock::duration(
+                    system_reference.load(std::memory_order_acquire)));
+        }
+
+        operator epoch_info() const noexcept
+        {
+            return epoch_info(get_system_reference(), steady_reference);
+        }
+
+        auto try_sync_with_system() noexcept -> bool;
+    };
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+    static global_epoch_info epoch_;
 
 public:
     using rep = std::uint64_t;
@@ -76,6 +103,10 @@ public:
                 internal_clock::now().time_since_epoch()));
     }
 
+    static auto try_sync_epoch() noexcept -> bool
+    {
+        return epoch_.try_sync_with_system();
+    }
     static auto epoch() noexcept -> epoch_info
     {
         return epoch_;
@@ -91,7 +122,8 @@ public:
                 duration_cast<internal_clock::duration>(t.time_since_epoch()));
 
         auto const internalSinceEpoch = internalTime - epoch_.steady_reference;
-        auto const systemTime = epoch_.system_reference + internalSinceEpoch;
+        auto const systemTime
+                = epoch_.get_system_reference() + internalSinceEpoch;
 
         using requested_duration = std::common_type_t<Duration, duration>;
         return time_point_cast<requested_duration>(systemTime);
@@ -102,7 +134,7 @@ public:
             -> std::chrono::time_point<log_clock,
                                        std::common_type_t<Duration, duration>>
     {
-        auto const systemSinceEpoch = t - epoch_.system_reference;
+        auto const systemSinceEpoch = t - epoch_.get_system_reference();
         auto const internalTime = epoch_.steady_reference + systemSinceEpoch;
 
         using requested_duration = std::common_type_t<Duration, duration>;
