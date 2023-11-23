ce2e3f9c4bb29eeaa142d3ad40c72e1ee51cd27e
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-11-23 19:14:40 +0100

    build: Update to copier-cpp template v0.3.2

---
+++ tools/toolchains/x64-macos-gcc-12.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+set(CMAKE_C_COMPILER gcc-12)
+set(CMAKE_CXX_COMPILER g++-12)
+set(CMAKE_OSX_SYSROOT /Applications/Xcode_15.0.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk)
+
+set(CMAKE_C_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
