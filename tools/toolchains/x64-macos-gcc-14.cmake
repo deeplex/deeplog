be6fa57e2ad68e7cbbeb01ae6eb6c609652f5166
Henrik S. Gaßmann <henrik@gassmann.onl>
2025-06-09 10:50:15 +0000

    ci: use GCC-14 for macOS to workaround TLS ld bug

--- tools/toolchains/x64-macos-gcc-14.cmake
+++ tools/toolchains/x64-macos-gcc-14.cmake
@@ -1,13 +1,11 @@
 set(CMAKE_SYSTEM_PROCESSOR AMD64)
 
-set(CMAKE_C_COMPILER gcc-12)
-set(CMAKE_CXX_COMPILER g++-12)
+set(CMAKE_C_COMPILER gcc-14)
+set(CMAKE_CXX_COMPILER g++-14)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 14.2 CACHE STRING "OSX deployment target")
 set(CMAKE_OSX_ARCHITECTURES x86_64 CACHE STRING "OSX target architectures")
 
 set(CMAKE_C_STANDARD 17)
 set(CMAKE_CXX_STANDARD 20)
 
-# the new linker currently segfaults with gcc (I believe this is fixed with gcc 14)
-set(CMAKE_CXX_FLAGS_INIT "-Wl,-ld_classic")
 set(CMAKE_CXX_SCAN_FOR_MODULES OFF)
