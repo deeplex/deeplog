36b6607f400f8c6e2a49a33fca70a96259174fc5
Henrik S. Gaßmann <henrik@gassmann.onl>
2025-06-10 10:14:31 +0000

    build: improve std::atomic_ref check

--- tools/cmake/CheckStdAtomicRefSupport.cmake
+++ tools/cmake/CheckStdAtomicRefSupport.cmake
@@ -6,9 +6,14 @@ function(check_std_atomic_ref_support OUT_VAR)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
     check_cxx_source_compiles([=[
-        #include <version>
+        #include <atomic>
         static_assert(__cpp_lib_atomic_ref >= 201806L);
-        int main() {}
+        int main() {
+            int v = 2;
+            std::atomic_ref<int> rv(v);
+            rv.store(0, std::memory_order::relaxed);
+            return rv.load(std::memory_order::relaxed);
+        }
     ]=] "${OUT_VAR}")
     set("${OUT_VAR}" "${${OUT_VAR}}" PARENT_SCOPE)
 endfunction()
