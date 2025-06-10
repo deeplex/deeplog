a378b8d0a85d40fad238de16e7b354237e9a6f0c
Henrik S. Gaßmann <henrik@gassmann.onl>
2025-06-10 10:35:37 +0000

    build: close version 0.0.0-beta.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021-2023 Henrik Steffen Gaßmann'
 author = 'Henrik Steffen Gaßmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0-beta.6'
+release = '0.0.0-beta.7'
 
 
 # -- General configuration ---------------------------------------------------
